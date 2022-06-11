86ccd35c107f3d7089607415f2958f525b48fcd1
Jerome <jeromelagbas@gmail.com>
2022-06-11 14:05:10 -0700

    Ice Cream Challenge

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+//Step 1
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//Step 2
+console.log(iceCreamFlavours[0]);
+
+//Step 3
+console.log(iceCreamFlavours[5]);
+
+//Step 4
+console.log(iceCreamFlavours.length);
